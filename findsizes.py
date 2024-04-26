3ae9c4fd1b1b007ecc5fc91b51fb12f0dd7c9e8f
Abdulaziz <37148753+azizalbastaki@users.noreply.github.com>
2024-04-26 12:29:41 +0400

    cleanup

--- findsizes.py
+++ findsizes.py
@@ -1,10 +1,8 @@
 import os
 models = (os.listdir("assets/furniture"))
-# make a panda3d app that loads every model in models, measures the size of the model, and saves in it a file along with the filename
 print(models)
 from panda3d.core import loadPrcFileData
 loadPrcFileData("", "window-type offscreen")
-from panda3d.core import *
 from direct.showbase.ShowBase import ShowBase
 
 class MyApp(ShowBase):
@@ -13,13 +11,11 @@ class MyApp(ShowBase):
         self.models = models
         self.sizes = {}
         for model in self.models:
-            # check if it's a .glb file
             if not model.endswith(".glb"):
                 continue
             self.model = loader.loadModel(f"assets/furniture/{model}")
             bounds = self.model.getTightBounds()
             size = bounds[1] - bounds[0]
-            twoDimensionalSize = Vec2(size.getX(), size.getY())
             scalesize = int(size.length() + 1)
             self.sizes[model] = scalesize
         with open("sizes.txt", 'w') as file:
